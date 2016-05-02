001bdd5618b54e968dd8625a0ef2571ce07e0077
Liam Ellis <liamhellis@gmail.com>
2016-05-02 13:47:34 -0700

    cleaned up

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,16 +1,64 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
+
 public class Task {
 
-	private String mDescription;
+  private String description;
+  private int id;
 
-	public Task (String description){
-		mDescription = description;
-	}
+  public Task(String description) {
+    this.description = description;
+  }
 
-	public String getDescription() {
-    	return mDescription;
+  public String getDescription() {
+    return description;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  public static List<Task> all() {
+    String sql = "SELECT id, description FROM tasks";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Task.class);
+    }
   }
 
 
 
-}
+  // public void completeTask(){
+  //   mCompleted = true;
+  // }
+  //
+  // public boolean isCompleted() {
+  //   return mCompleted;
+  // }
+  //
+  // public LocalDateTime getCreatedAt() {
+  //   return mCreatedAt;
+  // }
+  //
+  // public static ArrayList<Task> all() {
+  //   return instances ;
+  // }
+  //
+  //
+  //
+  // public static Task find(int id){
+  //   try{
+  //     return instances.get(id - 1);
+  //   } catch (IndexOutOfBoundsException e){
+  //     return null;
+  //   }
+  // }
+  //
+  // public static void clearTasksArray() {
+  //
+  //   //METHOD BELOW IS A BUILT IN METHOD
+  //   instances.clear();
+  // }
 
+}
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,18 +1,93 @@
+import java.time.LocalDateTime;
+import org.sql2o.*;
+
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TaskTest {
 
-	@Test
-	public void Task_instantiatesCorrectly_true() {
-	Task myTask = new Task("Mow the lawn");
-	assertEquals(true, myTask instanceof Task);
-	  }
-
-	@Test
-	public void Task_instantiatesWithDescription_String() {
-	Task myTask = new Task("Mow the lawn");
-	assertEquals("Mow the lawn", myTask.getDescription());
+  @Before
+  public void setUp() {
+      DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
   }
-	
-}   
+
+  @After
+  public void tearDown() {
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "DELETE FROM tasks *;";
+        con.createQuery(sql).executeUpdate();
+      }
+    }
+
+  @Rule
+  public ClearRule clearRule = new ClearRule();
+
+  @Test
+  public void task_instantiatesCorrectly_true() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(true, myTask instanceof Task);
+  }
+
+  @Test
+  public void task_instantiatesWithDescription_String() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals("Mow the lawn", myTask.getDescription());
+  }
+
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Task.all().size(), 0);
+  }
+
+  @Test
+  public void equals_returnsTrueIfDescriptionsAretheSame() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Mow the lawn");
+    assertTrue(firstTask.equals(secondTask));
+  }
+
+  // @Test
+  // public void isCompleted_isFalseAfterInstantiation_false() {
+  //   Task myTask = new Task("Mow the lawn");
+  //   assertEquals(false, myTask.isCompleted());
+  // }
+  //
+  // @Test
+  // public void getCreatedAt_instantiatesWithCurrentTime_today() {
+  //   Task myTask = new Task("Mow the lawn");
+  //   assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
+  // }
+  //
+  // @Test
+  // public void all_returnsAllInstancesOfTask_true() {
+  //   Task firstTask = new Task("Mow the lawn");
+  //   Task secondTask = new Task("Buy groceries");
+  //   assertTrue(Task.all().contains(firstTask));
+  //   assertTrue(Task.all().contains(secondTask));
+  // }
+  //
+  // @Test
+  // public void newId_tasksInstantiateWithAnID_true() {
+  //   Task myTask = new Task("Mow the lawn");
+  //   assertEquals(Task.all().size(), myTask.getId());
+  // }
+  //
+  // @Test
+  // public void find_returnsTaskWithSameId_secondTask() {
+  //   Task firstTask = new Task("Mow the lawn");
+  //   Task secondTask = new Task("Buy groceries");
+  //   assertEquals(Task.find(secondTask.getId()), secondTask);
+  // }
+  //
+  // @Test
+  // public void find_returnsNullWhenNoTaskFound_null() {
+  //   assertTrue(Task.find(999) == null);
+  // }
+  //
+  // @Test
+  // public void clear_emptiesAllTasksFromArrayList_0() {
+  //   Task myTask = new Task("Mow the lawn");
+  //   Task.clearTasksArray();
+  //   assertEquals(Task.all().size(), 0);
+  // }
+}
