599abf09e28ab4af694560f39642722c5e1628d3
Liam Ellis <liamhellis@gmail.com>
2016-05-02 20:09:43 -0700

    clean version, no redundant files

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,11 +2,23 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import org.sql2o.*;
+import java.sql.Timestamp;
 
 public class Task {
 
   private String description;
   private int id;
+  private Timestamp due_date;
+
+  @Override
+  public boolean equals(Object otherTask) {
+    if (!(otherTask instanceof Task)) {
+      return false;
+    } else {
+      Task newTask = (Task) otherTask;
+      return this.getDescription().equals(newTask.getDescription()) && this.getId() == newTask.getId();
+    }
+  }
 
   public Task(String description) {
     this.description = description;
@@ -25,6 +37,42 @@ public class Task {
     try(Connection con = DB.sql2o.open()) {
       return con.createQuery(sql).executeAndFetch(Task.class);
     }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO tasks (description, due_date) VALUES (:description, :due_date)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("description", this.description)
+        .addParameter("due_date", this.due_date)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Task> orderByDueDate() {
+    String sql = "SELECT description, due_date FROM tasks ORDER BY due_date";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Task.class);
+    }
+  }
+
+  public static Task find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM tasks where id=:id";
+      Task task = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Task.class);
+      return task;
+    }
+  }
+
+  public void setDueDate(Timestamp date) {
+    this.due_date = date;
+  }
+
+  public Timestamp getDueDate() {
+    return due_date;
   }
 
 
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,5 +1,7 @@
 import java.time.LocalDateTime;
 import org.sql2o.*;
+import java.util.List;
+import java.sql.Timestamp;
 
 import org.junit.*;
 import static org.junit.Assert.*;
@@ -19,8 +21,8 @@ public class TaskTest {
       }
     }
 
-  @Rule
-  public ClearRule clearRule = new ClearRule();
+  // @Rule
+  // public ClearRule clearRule = new ClearRule();
 
   @Test
   public void task_instantiatesCorrectly_true() {
@@ -44,6 +46,43 @@ public class TaskTest {
     Task firstTask = new Task("Mow the lawn");
     Task secondTask = new Task("Mow the lawn");
     assertTrue(firstTask.equals(secondTask));
+  }
+
+  @Test
+  public void save_returnsTrueIfDescriptionsAretheSame() {
+    Task myTask = new Task("Mow the lawn");
+    Timestamp timestamp = Timestamp.valueOf("2007-09-23 10:10:10.0");
+    myTask.setDueDate(timestamp);
+    myTask.save();
+    assertTrue(Task.all().get(0).equals(myTask));
+  }
+
+  @Test
+  public void find_findsTaskInDatabase_true() {
+    Task myTask = new Task("Mow the lawn");
+    Timestamp timestamp = Timestamp.valueOf("2007-09-23 10:10:10.0");
+    myTask.setDueDate(timestamp);
+    myTask.save();
+    Task savedTask = Task.find(myTask.getId());
+    assertTrue(myTask.equals(savedTask));
+  }
+
+  @Test
+  public void find_setDueDateInDatabase_true() {
+    Task myTask1 = new Task("Mow the lawn");
+    Task myTask2 = new Task("Walk the dog");
+    Task myTask3 = new Task("clean");
+    Timestamp timestamp = Timestamp.valueOf("2007-09-23 10:10:10.0");
+    Timestamp timestamp2 = Timestamp.valueOf("2008-09-23 10:10:10.0");
+    Timestamp timestamp3 = Timestamp.valueOf("2009-09-23 10:10:10.0");
+    myTask1.setDueDate(timestamp);
+    myTask2.setDueDate(timestamp2);
+    myTask3.setDueDate(timestamp3);
+    myTask2.save();
+    myTask1.save();
+    myTask3.save();
+    List<Task> newList = Task.orderByDueDate();
+    assertTrue(myTask1.getDescription().equals(newList.get(0).getDescription()));
   }
 
   // @Test
